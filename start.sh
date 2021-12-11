34e05470d80233e26f5ed779d56edd169ed6d097
Yijing Gao <71132150+Yijinggao@users.noreply.github.com>
2021-12-11 14:41:42 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,3 @@ mkdir -p /var/log;
 chmod -R 777 /var/log;
 mongod --fork --logpath=/var/log/mongodb.log;
 python3 data_acquire.py & python3 app.py;
-#python3 -c 'import pymongo;list(pymongo.MongoClient().get_database("energy").energy.find())';
